eb79516bb042d423319dbb93e94e288fe19598cc
natnaelengeda <nattynengeda@gmail.com>
2025-04-13 19:16:16 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: 'vite-app',
-      script: 'serve',
-      args: 'dist --single --listen 3000',
+      name: "vite-app",
+      script: "serve ./dist --single --listen 4040",
+      // args: 'dist --single --listen 3000',
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
     },
   ],
-};
+}
